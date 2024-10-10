c5c2293fd811511c63568fd6aba5a0d812cbb1d2
minhtai2911 <149917860+minhtai2911@users.noreply.github.com>
2024-10-10 20:09:55 +0700

    verify

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -4,9 +4,9 @@ import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = Router();
 
-router.get('/', userController.getAllUsers);
+router.get('/', authMiddleware.verifyToken, userController.getAllUsers);
 router.get('/:id', authMiddleware.verifyToken, userController.getUserById);
-router.delete('/:id', userController.deleteUserById);
-router.put('/:id', userController.updateUserById);
+router.delete('/:id', authMiddleware.verifyToken, userController.deleteUserById);
+router.put('/:id', authMiddleware.verifyToken, userController.updateUserById);
 
 export default router;
