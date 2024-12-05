659ed1e2a2bbf60683fb79610e83be203b88e2b5
minhtai2911 <149917860+minhtai2911@users.noreply.github.com>
2024-12-05 19:15:03 +0700

    feat: calculate total rating

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Review from "../models/review.js";
+import Product from "../models/product.js";
 
 const getAllReviews = asyncHandler(async (req, res, next) => {
   try {
@@ -17,19 +18,21 @@ const createReview = asyncHandler(async (req, res, next) => {
   try {
     const { userId, productId, rating, content } = req.body;
 
-    if (!userId || !productId || !rating || !content) {
+    if (!userId || !productId || !rating) {
       throw new Error("Please fill all required fields");
-    }
-
-    const existingReview = await Review.findOne({ userId, productId });
-
-    if (!existingReview) {
-      throw new Error("Review already exists");
     }
 
     const newReview = new Review({ userId, productId, rating, content });
 
     await newReview.save();
+
+    const product = await Product.findById(productId);
+    product.totalReview = product.totalReview + 1;
+    product.rating =
+      (rating + product.rating * (product.totalReview - 1)) /
+      product.totalReview;
+    await product.save();
+
     res.status(201).json(newReview);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -56,12 +59,22 @@ const updateReviewById = asyncHandler(async (req, res, next) => {
 
     const { userId, productId, rating, content } = req.body;
 
+    if (!userId || !productId || !rating)
+      throw new Error("Please fill all required fields");
+
+    const product = await Product.findById(productId);
+    product.rating =
+      (product.rating * product.totalReview - review.rating + rating) /
+      product.totalReview;
+    await product.save();
+
     review.userId = userId || review.userId;
     review.productId = productId || review.productId;
     review.rating = rating || review.rating;
     review.content = content || review.content;
 
     await review.save();
+
     res.status(200).json(review);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -73,6 +86,13 @@ const deleteReviewById = asyncHandler(async (req, res, next) => {
     const review = await Review.findByIdAndDelete(req.params.id);
 
     if (!review) return res.status(404).json({ message: "Review not found" });
+
+    const product = await Product.findById(review.productId);
+    product.totalReview = product.totalReview - 1;
+    product.rating =
+      (product.rating * (product.totalReview + 1) - review.rating) /
+      product.totalReview;
+    await product.save();
 
     res.status(200).json({ message: "Review deleted successfully" });
   } catch (err) {
