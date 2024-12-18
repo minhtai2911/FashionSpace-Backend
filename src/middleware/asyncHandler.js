56910a89e1e1d7c05a041ddcd49447afec8ec13a
minhtai2911 <149917860+minhtai2911@users.noreply.github.com>
2024-12-18 23:08:02 +0700

    update

--- src/middleware/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch((err) => {
-    return res.status(500).json({ error: err.message,
-        message: "Đã xảy ra lỗi, vui lòng thử lại!", });
-  });
-};
-
-export default asyncHandler;
